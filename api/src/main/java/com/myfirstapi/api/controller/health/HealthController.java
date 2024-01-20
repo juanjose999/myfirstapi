435a90933b733e8090fed7823839038c48cf2a2c
juanjose999 <juanjose000012@gmail.com>
2024-01-20 17:36:20 -0500

    advence exercise api

--- api/src/main/java/com/myfirstapi/api/controller/health/HealthController.java
+++ api/src/main/java/com/myfirstapi/api/controller/health/HealthController.java
@@ -1,4 +1,7 @@
 package com.myfirstapi.api.controller.health;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
@@ -6,5 +9,8 @@ public class HealthController {
     @RequestMapping("/health")
     public String checkAPI(){
         return "<h1>The API is working ok!</h1>";
+    }
+    public ResponseEntity<String> handleException(Exception ex){
+        return new ResponseEntity<>("Error en la api:" + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
